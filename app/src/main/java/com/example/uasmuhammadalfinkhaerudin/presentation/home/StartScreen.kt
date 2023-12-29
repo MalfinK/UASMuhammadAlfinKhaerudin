c85aff7a408a3e539186da95284b78eb5b2353f1
MalfinK <muhammadalfin1235678@gmail.com>
2023-12-29 16:13:21 +0700

    Update Code

--- app/src/main/java/com/example/uasmuhammadalfinkhaerudin/presentation/home/StartScreen.kt
+++ app/src/main/java/com/example/uasmuhammadalfinkhaerudin/presentation/home/StartScreen.kt
@@ -1,5 +1,6 @@
 package com.example.uasmuhammadalfinkhaerudin.presentation.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,9 +21,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uasmuhammadalfinkhaerudin.R
 
 @Composable
 fun StartScreen(
@@ -39,9 +42,17 @@ fun StartScreen(
             fontWeight = FontWeight.Bold,
             fontSize = 28.sp
         )
+        Spacer(modifier = Modifier.size(10.dp))
+        // tampilkan logo
+        Image(
+            painter = painterResource(id = R.mipmap.logo_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(500.dp)
+        )
         Spacer(modifier = Modifier.size(15.dp))
         Text(
-            text = "created by",
+            text = "Created By",
             fontWeight = FontWeight.SemiBold,
             fontSize = 20.sp
         )
